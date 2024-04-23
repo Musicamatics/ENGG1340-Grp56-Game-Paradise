89c0a054e59d645fd17ded5d24ea078111189532
LeungLongHey <164484769+LeungLongHey@users.noreply.github.com>
2024-04-23 22:53:09 +0800

    Update StartingGame.cpp
    Add function user name

--- StartingGame.cpp
+++ StartingGame.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-
+void print(string str)
+{
+    cout << str << endl;
+}
 
 int main()
 {
+    bool input_check=true;
+    string user;
+    print("Welcome to Snake Game!");
+    print("Please enter your name:");
+    while (input_check)
+    {
+        getline(cin,user);
+        if (user!="")
+        {
+            input_check=false;
+        }
+    }
+    return 0;
 }
+      
+
