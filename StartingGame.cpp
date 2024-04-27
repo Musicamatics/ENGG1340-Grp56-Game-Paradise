1ccf469fcfed56ba7f746fe68529189b902d6069
LeungLongHey <164484769+LeungLongHey@users.noreply.github.com>
2024-04-27 19:10:58 +0800

    Update StartingGame.cpp
    Add choosing game

--- StartingGame.cpp
+++ StartingGame.cpp
@@ -11,7 +11,7 @@ int main()
 {
     bool input_check=true;
     string user;
-    print("Welcome to Snake Game!");
+    print("Welcome to GameParadise!");
     print("Please enter your name:");
     while (input_check)
     {
@@ -21,6 +21,31 @@ int main()
             input_check=false;
         }
     }
+
+    bool choose=true;
+    string chosen;
+    while (choose)
+    {
+        print("Please choose a game to play:");
+        print("1) Snake Game (Enter 1)");
+        print("2) Wordle     (Enter 2)");
+        getline(cin, chosen);
+        if (chosen!="1" && chosen!="2")
+        {
+            print("Invalid Input!!");
+            print("Please try again!");
+        }
+        else if (chosen=="1")
+        {
+            print("Loading Snake Game...");
+            choose=false;
+        }
+        else
+        {
+            print("Loading Wordle")
+            choose=false;
+        }
+        
     return 0;
 }
       
