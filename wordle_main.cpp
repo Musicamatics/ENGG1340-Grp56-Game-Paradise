69b37e9b955843e60afa24d669065a9f4a27249c
yburnachs <166601909+yburnachs@users.noreply.github.com>
2024-04-30 17:26:32 +0800

    Update wordle_main.cpp

--- wordle_main.cpp
+++ wordle_main.cpp
@@ -1,25 +1,20 @@
-#include <iostream>
-#include <stdlib.h>
-#include <cstdlib>
-#include <fstream>
-#include <time.h>
-#include <ctime>
-#include <map>
-#include <list>
-#include <random>
 #include "wordle.h"
-#include <unistd.h>
-using namespace std;
 
 int main() {
     bool match;
-    int len_of_words = 5; num_of_tries = 6; currentTry = 0; num_of_games = 0, score = 0;
+    int len_of_words = 5;
+    int num_of_tries = 6;
+    int currentTry = 0;
+    int num_of_games = 0, score = 0;
 
-    vector <vector<char>> charr; vector <vector<bool>> found; vector <string> tries;
-    string input1, input2, input3, word;
-    map<dictPair, string> mydict;
-    string filepath = "/usr/share/dict/words"; /*access system dictionary*/
-    bool b = loaddict(filepath, mydict), present = false;
+    std::vector<std::vector<char>> charr;
+    std::vector<std::vector<bool>> found;
+
+    std::vector <std::string> tries;
+    std::string input1, input2, input3, word;
+    std::map<dictPair, std::string> mydict;
+    std::string filepath = "/usr/share/dict/words"; /*access system dictionary*/
+    bool b = loaddict(filepath, mydict);
     char randletter;
 
     gameInstructions();
@@ -29,8 +24,8 @@ int main() {
                 input1 = "Y";
                 continue;
             }
-            cout << "Would you like to start playing Wordle? Type Y to start or Q to quit: ";
-            getline(cin, input1);
+            std::cout << "Would you like to start playing Wordle? Type Y to start or Q to quit: ";
+            std::getline(std::cin, input1);
         }while(input1 != "Y" && input1 != "Q");
         if(input1 == "Q"){
             break;
@@ -47,53 +42,59 @@ int main() {
                 }while(overlapWord); /*if landmine appears in guessing word, the landmine letter is regenerated until it is not in the word*/
             }
             currentTry = 0;
-            bool present = false;
+            int present = -1;
             while(currentTry < num_of_tries){
                 do{
-                    cout << "Please enter your guess (word length must be 5) or type Q to quit: ";
-                    getline(cin, input2);
+                    std::cout << "Please enter your guess (word length must be 5) or type Q to quit: ";
+                    std::getline(std::cin, input2);
                     if(input2 == "Q"){
                         break;
                     }
                     toLowerCase(input2);
                     
-                    present = wordpresent(randletter, input2); /*detect whether user input includes landmine letter*/
-                    if(present){
-                        triggerLandmine(randletter);
-                        break;
-                    }
-                    
                     while(!dictword(input2, mydict)){ /*check whether user input word is valid*/
+                        if(input2 == "Q"){
+                            break;
+                        }
                         if(input2.length() != 5){
-                            cout << "Please enter a word that has 5 letters: " ;
+                            std::cout << "Please enter a word that has 5 letters: " ;
                         }
                         else{
-                            cout << "Please enter a valid word found in the dictionary: " ;
+                            std::cout << "Please enter a valid word found in the dictionary: " ;
                         }
-                        getline(cin, input2);
+                        getline(std::cin, input2);
                     }
                 }while(!isValid(input2) || input2 == "Q");
-                if(present || input2 == "Q"){
+                if(input2 == "Q"){
                     break;}
-                
+                present = wordpresent(randletter, input2); 
                 match = testingdoWordsMatch(word, input2); /*check whether user input matches guessed word*/
                 playwordle(word, input2, charr, found, currentTry, tries); /*start wordle game*/
-                printResults(charr, found, tries); /*print display of previous attempts*/
+                printResults(charr, found, tries, present); /*print display of previous attempts*/
+                /*detect whether user input includes landmine letter*/
+                if(present != -1){
+                    triggerLandmine(randletter);
+                    break;
+                    }
                 currentTry++;
                 if(match == 1){
                     endgame(currentTry);
                     break;
                 }
-                if(currentTry != 6){cout << "You have " << 6-currentTry << " tries left!" << endl;}
+                if(currentTry != 6){
+                    std::cout << "You have " << 6-currentTry << " tries left!\n";}
             }
             earlyEndgame(currentTry, input2, present, word, match);
             do{
                 sleep (1);
-                cout << "Would you like to play again? Enter Y for yes and Q for no: " << endl;
-                getline(cin, input3);
+                std::cout << "Would you like to play again? Enter Y for yes and Q for no: ";
+                getline(std::cin, input3);
             }while(input1 != "Y" && input1 != "Q");
+            num_of_games ++; 
+            writefile(num_of_games, match, currentTry, word, present);
+            continuegame(currentTry,tries, found, charr, match);
+            std::cout << num_of_games << "/n";
             if(input3 == "Y"){
-                continuegame(currentTry,tries, found, charr, num_of_games, match, score);
                 continue;
             }
             else if(input3 == "Q"){
@@ -102,6 +103,6 @@ int main() {
 
         }
     };
-    final(score);
+    outfile();
     return 0;
 }
